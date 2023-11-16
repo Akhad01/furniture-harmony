12c29eb8da9e52f3bbc061f47aa18eee6a95a2b8
Akhad <abdurazakovahad@gmail.com>
2023-11-16 20:38:28 +0300

    added Middleware that checks the user's role

--- server/routes/furnitureRouter.js
+++ server/routes/furnitureRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = Router()
 const furnitureContoller = require('../controllers/furnitureController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', furnitureContoller.create)
+router.post('/', checkRole('ADMIN'), furnitureContoller.create)
 router.get('/', furnitureContoller.getAll)
 router.get('/:id', furnitureContoller.getOne)
 
